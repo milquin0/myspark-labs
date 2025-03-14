d8239ffb8599c276dffbf946ff1724545c73881a
milquin0 <211u0382@alumno.itssat.edu.mx>
2025-03-14 12:13:09 -0600

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
 from pyspark.sql import SparkSession
 
 # Create a spark session
-spark = SparkSession.builder.appName("HelloWorld").getOrCreate()
+spark = SparkSession.builder.appName("HelloWorld").getOrCreate() 
 
 # Use sql() to write a raw SQL query
 df = spark.sql("SELECT 'Hello World' as hello")
